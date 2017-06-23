0f7b1afffe72ae17cfc28d81c57b916c5f5568ef
ironcoconut <ironcoconut@gmail.com>
2017-06-23 02:45:46 -0500

    Add and configure Devise Invitable Gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :invitable, :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable
 end
